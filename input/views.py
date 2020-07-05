5c785c2723a902ab9316b0e549d65e50fa3116e0
ggrp-china <835276024@qq.com>
2020-07-06 03:45:48 +0800

    更新

--- input/views.py
+++ input/views.py
@@ -21,12 +21,14 @@ class FileRecordsView(APIView):
 
     def post(self,request):
         csv_file = request.FILES.get('csv')
-        handle_uploaded_file(csv_file)
         if (csv_file) is None:
             return Response({'detail': '文件为空'}, status=status.HTTP_400_BAD_REQUEST)
         name = csv_file.name.split('.')[0]
         ext = csv_file.name.split('.')[-1].lower()
         if ext != "csv":
             return Response({'detail': '只能上传csv格式的文件'}, status=status.HTTP_400_BAD_REQUEST)
+        if not (csv_file.name[:-4]).isdigit():
+            return Response({'detail': '请上传的文件为序号，例：“0.csv”,否则无法正确排序'}, status=status.HTTP_400_BAD_REQUEST)
+        handle_uploaded_file(csv_file)
         csv = FileRecord.objects.create(name=csv_file.name, upload=csv_file)
         return Response()
